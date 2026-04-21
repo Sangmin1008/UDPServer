5ceaeddac0cff62237d464486ce8b3e79a5c7936
Sangmin1008 <sangmin11119@gmail.com>
2026-04-21 18:12:12 +0900

    feat : ServerConfig, PlayerData 클래스 생성

---
+++ UDPServer/Core/ServerConfig.cs
@@ -0,0 +1,21 @@
+﻿namespace UDPServer.Core;
+
+public class ServerConfig
+{
+    public string ServerIP { get; set; }
+    public int ServerPort { get; set; }
+    public int MaxPlayers { get; set; }
+    public int BufferSize { get; set; } // 1024 bytes
+    public int WorkerThreadCount { get; set; }
+    public int PlayerTimeoutSeconds { get; set; }
+
+    public ServerConfig()
+    {
+        ServerIP = "127.0.0.1";
+        ServerPort = 7777;
+        MaxPlayers = 100;
+        BufferSize = 1024;
+        WorkerThreadCount = Environment.ProcessorCount;
+        PlayerTimeoutSeconds = 30;
+    }
+}
---
+++ UDPServer/Models/PlayerData.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+using System.Numerics;
+
+namespace UDPServer.Models;
+
+public class PlayerData
+{
+    public int PlayerId { get; set; }
+    public Vector3 Position { get; set; }
+    public Vector3 Rotation { get; set; }
+    public IPEndPoint EndPoint { get; set; }
+    public DateTime LastUpdateTime { get; set; }
+
+    public PlayerData(int playerId, IPEndPoint endPoint)
+    {
+        PlayerId = playerId;
+        EndPoint = endPoint;
+        Position = Vector3.Zero;
+        Rotation = Vector3.Zero;
+        LastUpdateTime = DateTime.UtcNow;
+    }
+    
+    // 플레이어의 위치와 회전 정보를 업데이트 하는 메서드
+    public void UpdateTransform(Vector3 position, Vector3 rotation)
+    {
+        Position = position;
+        Rotation = rotation;
+    }
+}
